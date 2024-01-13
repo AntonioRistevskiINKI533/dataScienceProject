2818f82cc61b9cbbb19dccfc8a5a0a3c3ab8151e
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2024-01-13 21:33:24 +0100

    Convert date to month period

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ store_sales = store_sales.drop(['store','item'], axis=1)
 
 store_sales['date'] = pd.to_datetime(store_sales['date'])
 store_sales.info()
+
+store_sales['date'] = store_sales['date'].dt.to_period("M")
+# print(store_sales.head(10))
+monthly_sales = store_sales.groupby('date').sum().reset_index()
+# print(monthly_sales.head(10))
+monthly_sales.info()
+
+monthly_sales['date'] = monthly_sales['date'].dt.to_timestamp()
+# print(monthly_sales.head(10))
+monthly_sales.info()
