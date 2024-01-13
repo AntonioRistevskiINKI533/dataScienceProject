78af68b2d26abc74450cc587275caf313d73eb5c
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2024-01-13 21:28:59 +0100

    data file, drop store and item

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 store_sales = pd.read_csv("data.csv")
-print(store_sales.head(10))
-store_sales.info()
+# print(store_sales.head(10))
+# store_sales.info()
 
 store_sales = store_sales.drop(['store','item'], axis=1)
+# store_sales.info()
+
+store_sales['date'] = pd.to_datetime(store_sales['date'])
 store_sales.info()
